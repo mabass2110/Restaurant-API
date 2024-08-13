75bbfe7a953977ca9d06b1b46913445d960603a9
helder mabasso <hfmabasso@gmail.com>
2024-08-13 14:26:27 +0200

    body-parser middleware and a /restaurant post request

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./swagger');
 const app = express();
 const port = 3000;
+const bodyParser = require("body-parser");
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 //mocked data
 var restaurants = [
      {id: 0, name:"Burako da Velha"},
@@ -17,6 +25,30 @@ app.get("/restaurants", (req,res) => {
     res.send(restaurants);
 })
 
+app.post("/restaurant", (req, res) => {
+  const restaurant = {
+    id: req.body.id,
+    name: req.body.name
+  }
+
+  //checks if the id is a number and the name is a string
+  if( typeof restaurant.id === "number" && typeof restaurant.name === "string") {
+    restaurants.push(restaurant);
+    return res.status(201).json({
+        status: 201,
+        message: "Restaurant successfully created"
+      });
+  };
+      //error response
+      return res.status(400).json({
+        status: 400,
+        message: "Select a valid name and id."
+      })
+
+
+}
+)
+
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port} 🚀`);
 });
