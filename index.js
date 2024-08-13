e9f9c49a1150e5a62d4d31856ff9ab318e6d1418
helder mabasso <hfmabasso@gmail.com>
2024-08-13 12:15:44 +0200

    mocked data, and get restaurants route

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./swagger');
-
 const app = express();
 const port = 3000;
+//mocked data
+var restaurants = [
+     {id: 0, name:"Burako da Velha"},
+     {id: 1, name:"South Beach"},
+     {id: 3, name:"Cais66"}
+    ]
 
 // Serve Swagger API docs
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
+app.get("/restaurants", (req,res) => {
+    res.send(restaurants);
+})
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
